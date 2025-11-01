96505876c4839e0a745789f6e76361a9520379ff
Aleksandr Smirnov <genesboard@gmail.com>
2025-11-01 09:48:04 +0300

    add rector-laravel

---
+++ rector.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\CodingStyle\Rector\Stmt\RemoveUselessAliasInUseStatementRector;
+use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNewArrayRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\ValueObject\PhpVersion;
+use RectorLaravel\Rector\ClassMethod\ScopeNamedClassMethodToScopeAttributedClassMethodRector;
+use RectorLaravel\Rector\MethodCall\ValidationRuleArrayStringValueToArrayRector;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    // Paths to analyze
+    $rectorConfig->paths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        // __DIR__.'/tests',
+    ]);
+
+    // Enable caching for Rector
+    $rectorConfig->cacheDirectory(__DIR__.'/storage/rector');
+    $rectorConfig->cacheClass(FileCacheStorage::class);
+
+    // Import Rector sets for Laravel and general code quality
+    $rectorConfig->importNames();
+
+    // Define the Rector rules to apply
+    $rectorConfig->rules([
+        DeclareStrictTypesRector::class,
+        CompactToVariablesRector::class,
+        ValidationRuleArrayStringValueToArrayRector::class,
+        ScopeNamedClassMethodToScopeAttributedClassMethodRector::class,
+        SeparateMultiUseImportsRector::class,
+        RemoveUselessAliasInUseStatementRector::class,
+        NewlineAfterStatementRector::class,
+        CatchExceptionNameMatchingTypeRector::class,
+        ReturnTypeFromStrictNewArrayRector::class,
+    ]);
+
+    // Apply sets for Laravel and general code quality
+    $rectorConfig->sets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+        SetList::PHP_83,
+        SetList::INSTANCEOF,
+        SetList::CODING_STYLE,
+        SetList::CODE_QUALITY,
+    ]);
+
+    // Define PHP version for Rector
+    $rectorConfig->phpVersion(PhpVersion::PHP_83);
+};
